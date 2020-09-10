4557370cd7144eb19904e8cc4c81885c4fec9c74
Ashley Berry <ashleywberry@gmail.com>
2020-09-10 10:49:13 -0500

    updated PUT

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,9 @@ $( document ).ready( onReady );
 function onReady(){
     getSongs();
     $( '#addSongButton' ).on( 'click', addSong );
+    $( document ).on( 'click', '.deleteSongBtn', deleteSong );
+    $( document ).on( 'click', '.rankUpBtn', rankUp );
+    $( document ).on( 'click', '.rankDownBtn', rankDown );
 } // end onReady
 
 function addSong(){
@@ -18,6 +21,7 @@ function addSong(){
         data: objectToSend
     }).then( function( response ){
         console.log( 'back from POST with:', response );
+        getSongs();
     }).catch( function( err ){
         alert( 'error!' );
         console.log( err );
@@ -39,6 +43,9 @@ function getSongs(){
             ${ response[i].track }
             ${ response[i].artist }
             ${ response[i].published.split( 'T' )[0] }
+            <button class='deleteSongBtn' data-id=${response[i].id}>Delete</button>
+            <button class='rankUpBtn' data-id="${response[i].id}">Up</button>
+            <button class='rankDownBtn' data-id='${response[i].id}'>Down</button>
             </li>`)
         } // end for
     }).catch( function( err ){
@@ -46,3 +53,56 @@ function getSongs(){
         console.log( err );
     }) // end AJAX GET
 } // end getSongs()
+
+function deleteSong(){
+    // grab the 'data-id' attribute from our 'button'
+    console.log( 'in deleteSong' );
+    let songId = $(this).data('id');
+    $.ajax({
+        method: 'DELETE',
+        url: `/songs/${songId}` // convention for how we pass through IDs
+    }).then( function( response ){
+        console.log( 'Deleted!', response )
+        // refresh page (aka do another GET request)
+        getSongs();
+    }).catch( function( err ){
+        console.log( 'error in delete', err )
+        alert('oh noes!');
+    }) // end AJAX
+} // end deleteSong
+
+function rankUp(){
+    let songId = $(this).data('id');
+    console.log('up', songId);
+    $.ajax({
+        method: 'PUT', // means edit
+        url: `/songs/${songId}`,
+        data: { // data to send
+            direction: 'up'
+        }
+    }).then( function ( response ){
+        console.log( 'response from rankUp:', response );
+        getSongs();
+    }).catch( function( err ){
+        console.log( 'error in rankUp', err )
+        alert('oh noes!');
+    }) // end AJAX
+} // end rankUp
+
+function rankDown(){
+    let songId = $(this).data('id');
+    console.log('down', songId);
+    $.ajax({
+        method: 'PUT', // means edit
+        url: `/songs/${songId}`,
+        data: { // data to send
+            direction: 'down'
+        }
+    }).then( function ( response ){
+        console.log( 'response from rankUp:', response );
+        getSongs();
+    }).catch( function( err ){
+        console.log( 'error in rankUp', err )
+        alert('oh noes!');
+    }) // end AJAX
+} // end rankDown
--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.get( '/songs', ( req, res )=>{
     console.log( 'in /songs GET' );
     // test query: top 40 songs by rank
     // SELECT * FROM "songs" ORDER BY "rank" DESC LIMIT 40;
-    const queryString = 'SELECT * FROM "songs" ORDER BY "rank" DESC LIMIT 40;';
+    const queryString = 'SELECT * FROM "songs" ORDER BY "rank" ASC LIMIT 40;';
     pool.query( queryString ).then( ( results )=>{
         res.send( results.rows ); //runs if query was successful
     }).catch( ( err )=>{
@@ -43,6 +43,50 @@ app.get( '/songs', ( req, res )=>{
 app.post( '/songs', ( req, res )=>{
     console.log( 'in /songs POST:', req.body );
     // create query string
+    const queryString = `INSERT INTO "songs" (rank, artist, track, published) VALUES ( $1, $2, $3, $4 )`;
     // ask pool to run our Query String
-    res.send( 'chirp' );
+    pool.query( queryString, [ req.body.rank, req.body.artist, req.body.track, req.body.published ] ).then( ( results )=>{
+        res.sendStatus( 201 ); // success, created
+    }).catch( ( err ) =>{
+        console.log( err );
+        res.sendStatus( 500 );
+    }) // end query
 }) // end /songs POST
+
+app.delete( '/songs/:id', ( req, res )=>{
+    //grab the :id param from the URL
+    let songId = req.params.id;
+    const queryString = `DELETE FROM "songs" WHERE "id" = $1;`;
+    console.log( 'going to delete songs with id=', songId );
+    // execute DB query
+    pool.query( queryString, [ songId ] )
+    .then( ( response ) => {
+        console.log( 'deleted song!' );
+        res.sendStatus( 200 ); // ALWAYS need some kind of response. 200 would send back OK
+    }).catch( ( err ) => {
+        console.log( 'error deleting song', err );
+        res.sendStatus( 500 );
+    }) // end catch
+}) // end delete song
+
+app.put( '/songs/:id', ( req, res )=>{
+    // param is what song to do that to. 
+    // req.body is data - object that contains data whether going up or down
+    console.log( 'params', req.params.id, req.body ); 
+    let queryString = '';
+    if( req.body.direction === 'up' ){
+        queryString = `UPDATE "songs" SET "rank" = "rank"+1 WHERE "id" = $1`; // first put ${req.params.id} instead of $1 to test
+    } else if (req.body.direction === 'down'){
+        queryString = `UPDATE "songs" SET "rank" = "rank"-1 WHERE "id" = $1`;
+    } else {
+        console.log( 'send better data' );
+    }
+    pool.query( queryString, [ req.params.id ] ) // first used just ( queryString ) when testing, then added [r.p.i]
+    .then(( result ) => {
+        console.log( 'result from PUT:', result );
+        res.sendStatus( 200 );
+    }).catch(( err ) => {
+        console.log( 'result from PUT:', err );
+        res.sendStatus( 500 )
+    }) 
+})// end edit rank
